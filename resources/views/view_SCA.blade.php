cab0ffdf3b70258a5569e0f677eae66a12546f59
Angie3098 <angie23qs@gmail.com>
2021-11-09 15:09:47 -0600

    instituciones

---
+++ resources/views/view_SCA.blade.php
@@ -0,0 +1,257 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/style_SCA.css')}}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.css">
+    <title>SCA</title>
+</head>
+<body>
+  
+    <div class="container-fluid bg-dark text-light">
+        <h1 class="text-center p-2"><i class="fas fa-user-cog"></i> Instituciones</h1>
+    </div>
+
+    <div class="container-fluid mt-5">
+
+        <div class="container mb-3">
+            <button class="btn btn-primary">+ Nueva</button>
+            <button class="btn btn-success">+ Planillas</button>
+        </div>
+
+        <div class="container">
+            <table class="table table-bordered" id="tabla">
+                <thead class="bg-dark text-light">
+                    <tr>
+                        <th>Código</th>
+                        <th>Nombre</th>
+                        <th>Estado</th>
+                        <th>Dirección</th>
+                        <th>Contacto Directo</th>
+                        <th>Teléfono</th>
+                        <th style="width: 10%" class="text-center">Acción</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>AICE-123</td>
+                        <td>ICE</td>
+                        <td>Activo</td>
+                        <td>Siquirres</td>
+                        <td>ice@gmail.com</td>
+                        <td>64451852</td>
+                        <td>
+                            <div class="clearfix">
+                                <div class="float-start">
+                                    <button class="btn btn-warning"><i class="fas fa-edit text-light"></i></button>
+                                </div>
+                                
+                                <div class="float-end">
+                                    <button onclick="return confirm ('¿Desea eliminar la institución ICE?')" class="btn btn-danger"><i class="fas fa-trash"></i></button>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>AICE-123</td>
+                        <td>AWE</td>
+                        <td>Activo</td>
+                        <td>Siquirres</td>
+                        <td>ice@gmail.com</td>
+                        <td>64451852</td>
+                        <td>
+                            <div class="clearfix">
+                                <div class="float-start">
+                                    <button class="btn btn-warning"><i class="fas fa-edit text-light"></i></button>
+                                </div>
+                                
+                                <div class="float-end">
+                                    <button onclick="return confirm ('¿Desea eliminar la institución ICE?')" class="btn btn-danger"><i class="fas fa-trash"></i></button>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>AICE-123</td>
+                        <td>ICE</td>
+                        <td>Activo</td>
+                        <td>Siquirres</td>
+                        <td>ice@gmail.com</td>
+                        <td>64451852</td>
+                        <td>
+                            <div class="clearfix">
+                                <div class="float-start">
+                                    <button class="btn btn-warning"><i class="fas fa-edit text-light"></i></button>
+                                </div>
+                                
+                                <div class="float-end">
+                                    <button onclick="return confirm ('¿Desea eliminar la institución ICE?')" class="btn btn-danger"><i class="fas fa-trash"></i></button>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>AICE-123</td>
+                        <td>ICE</td>
+                        <td>Activo</td>
+                        <td>Siquirres</td>
+                        <td>ice@gmail.com</td>
+                        <td>64451852</td>
+                        <td>
+                            <div class="clearfix">
+                                <div class="float-start">
+                                    <button class="btn btn-warning"><i class="fas fa-edit text-light"></i></button>
+                                </div>
+                                
+                                <div class="float-end">
+                                    <button onclick="return confirm ('¿Desea eliminar la institución ICE?')" class="btn btn-danger"><i class="fas fa-trash"></i></button>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>AICE-123</td>
+                        <td>ICE</td>
+                        <td>Activo</td>
+                        <td>Siquirres</td>
+                        <td>ice@gmail.com</td>
+                        <td>64451852</td>
+                        <td>
+                            <div class="clearfix">
+                                <div class="float-start">
+                                    <button class="btn btn-warning"><i class="fas fa-edit text-light"></i></button>
+                                </div>
+                                
+                                <div class="float-end">
+                                    <button onclick="return confirm ('¿Desea eliminar la institución ICE?')" class="btn btn-danger"><i class="fas fa-trash"></i></button>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>AICE-123</td>
+                        <td>ICE</td>
+                        <td>Activo</td>
+                        <td>Siquirres</td>
+                        <td>ice@gmail.com</td>
+                        <td>64451852</td>
+                        <td>
+                            <div class="clearfix">
+                                <div class="float-start">
+                                    <button class="btn btn-warning"><i class="fas fa-edit text-light"></i></button>
+                                </div>
+                                
+                                <div class="float-end">
+                                    <button onclick="return confirm ('¿Desea eliminar la institución ICE?')" class="btn btn-danger"><i class="fas fa-trash"></i></button>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>AICE-123</td>
+                        <td>ICE</td>
+                        <td>Activo</td>
+                        <td>Siquirres</td>
+                        <td>ice@gmail.com</td>
+                        <td>64451852</td>
+                        <td>
+                            <div class="clearfix">
+                                <div class="float-start">
+                                    <button class="btn btn-warning"><i class="fas fa-edit text-light"></i></button>
+                                </div>
+                                
+                                <div class="float-end">
+                                    <button onclick="return confirm ('¿Desea eliminar la institución ICE?')" class="btn btn-danger"><i class="fas fa-trash"></i></button>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>AICE-123</td>
+                        <td>ICE</td>
+                        <td>Activo</td>
+                        <td>Siquirres</td>
+                        <td>ice@gmail.com</td>
+                        <td>64451852</td>
+                        <td>
+                            <div class="clearfix">
+                                <div class="float-start">
+                                    <button class="btn btn-warning"><i class="fas fa-edit text-light"></i></button>
+                                </div>
+                                
+                                <div class="float-end">
+                                    <button onclick="return confirm ('¿Desea eliminar la institución ICE?')" class="btn btn-danger"><i class="fas fa-trash"></i></button>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>AICE-123</td>
+                        <td>ICE</td>
+                        <td>Activo</td>
+                        <td>Siquirres</td>
+                        <td>ice@gmail.com</td>
+                        <td>64451852</td>
+                        <td>
+                            <div class="clearfix">
+                                <div class="float-start">
+                                    <button class="btn btn-warning"><i class="fas fa-edit text-light"></i></button>
+                                </div>
+                                
+                                <div class="float-end">
+                                    <button onclick="return confirm ('¿Desea eliminar la institución ICE?')" class="btn btn-danger"><i class="fas fa-trash"></i></button>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>AICE-123</td>
+                        <td>ICE</td>
+                        <td>Activo</td>
+                        <td>Siquirres</td>
+                        <td>ice@gmail.com</td>
+                        <td>64451852</td>
+                        <td>
+                            <div class="clearfix">
+                                <div class="float-start">
+                                    <button class="btn btn-warning"><i class="fas fa-edit text-light"></i></button>
+                                </div>
+                                
+                                <div class="float-end">
+                                    <button onclick="return confirm ('¿Desea eliminar la institución ICE?')" class="btn btn-danger"><i class="fas fa-trash"></i></button>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    {{--  JS  --}}
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.js"></script>
+    <script>
+        $(document).ready( function () {
+            $('#tabla').DataTable({
+                pageLength : 5,
+                lengthMenu: [[5, 10, 20, -1], [5, 10, 20, 'Todos']],
+                bInfo: false,
+                bLengthChange:false,
+            });
+        } );
+    </script>
+</body>
+</html>
